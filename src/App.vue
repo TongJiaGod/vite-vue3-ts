94d028c0d8140bb49e1187c416b967bb38b74b18
tony <459918037@qq.com>
2025-07-01 22:51:11 +0800

    feat(1.0.0): 初始化项目

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="app"></div>
+</template>
+
+<style scoped lang="scss">
+a {
+  width: 100px;
+}
+
+div {
+  height: 100px;
+}
+</style>
---
+++ src/views/index.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="content">content</div>
+</template>
+
+<style scoped lang="scss">
+.content {
+  width: 100%;
+}
+</style>
